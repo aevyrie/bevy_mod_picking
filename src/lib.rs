d176c0c75c534285c18ff25863c83726d5848e29
Aevyrie <aevyrie@gmail.com>
2020-08-30 21:57:26 -0700

    Fixed excess None to None hover state updates

--- src/lib.rs
+++ src/lib.rs
@@ -274,7 +274,7 @@ fn cursor_pick(
             //println!("No collision in {}", mesh_handle.id.0);
         }
     }
-    if !hit_found {
+    if !hit_found && pick_state.hovered != None {
         pick_state.hovered_previous = pick_state.hovered;
         pick_state.hovered = None;
         println!("{:?} to {:?}", pick_state.hovered_previous, pick_state.hovered);
