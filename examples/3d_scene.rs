84a0525e538b20e645b12c5d6a6fb7d739ae3291
Aevyrie <aevyrie@gmail.com>
2020-09-03 22:06:35 -0700

    Added example

---
+++ examples/3d_scene.rs
@@ -0,0 +1,76 @@
+use bevy::prelude::*;
+use bevy_mod_picking::*;
+
+fn main() {
+    App::build()
+        .add_resource(Msaa { samples: 4 })
+        .add_default_plugins()
+        .add_plugin(PickingPlugin)
+        .add_startup_system(setup.system())
+        .add_system(get_picks.system())
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let camera_entity = Entity::new();
+    // add entities to the world
+    commands
+        // plane
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+            material: materials.add(Color::rgb(0.1, 0.2, 0.1).into()),
+            ..Default::default()
+        })
+        .with(PickableMesh::new(camera_entity))
+        .with(HighlightablePickMesh::new())
+        .with(SelectablePickMesh::new())
+        // cube
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
+            translation: Translation::new(0.0, 1.0, 0.0),
+            ..Default::default()
+        })
+        .with(PickableMesh::new(camera_entity))
+        .with(HighlightablePickMesh::new())
+        .with(SelectablePickMesh::new())
+        // sphere
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                subdivisions: 4,
+                radius: 0.5,
+            })),
+            material: materials.add(Color::rgb(0.1, 0.4, 0.8).into()),
+            translation: Translation::new(1.5, 1.5, 1.5),
+            ..Default::default()
+        })
+        .with(PickableMesh::new(camera_entity))
+        .with(HighlightablePickMesh::new())
+        .with(SelectablePickMesh::new())
+        // light
+        .spawn(LightComponents {
+            translation: Translation::new(4.0, 8.0, 4.0),
+            ..Default::default()
+        })
+        // camera
+        .spawn_as_entity(camera_entity, Camera3dComponents {
+            transform: Transform::new_sync_disabled(Mat4::face_toward(
+                Vec3::new(-3.0, 5.0, 8.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 1.0, 0.0),
+            )),
+            ..Default::default()
+        });
+}
+
+fn get_picks(
+    pick_state: ResMut<PickState>,
+) {
+    println!("All entities:\n{:?}", pick_state.list());
+    println!("Top entity:\n{:?}", pick_state.top());
+}
