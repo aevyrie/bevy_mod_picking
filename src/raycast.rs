a1ab493747b40daf36a0f0f31f14f3b78c86ed91
Aevyrie <aevyrie@gmail.com>
2020-09-13 19:58:20 -0700

    Moller Trumbore implementation

--- src/raycast.rs
+++ src/raycast.rs
@@ -6,19 +6,19 @@ use bevy::{
 
 #[derive(Debug, PartialOrd, PartialEq, Copy, Clone)]
 pub struct Ray3D {
-    position: Vec3,
+    origin: Vec3,
     direction: Vec3,
 }
 
 impl Ray3D {
     pub fn new(position: Vec3, direction: Vec3) -> Self {
         Ray3D {
-            position,
+            origin: position,
             direction,
         }
     }
     pub fn position(&self) -> Vec3 {
-        self.position
+        self.origin
     }
     pub fn direction(&self) -> Vec3 {
         self.direction
@@ -27,9 +27,9 @@ impl Ray3D {
 
 #[derive(Debug, PartialOrd, PartialEq, Copy, Clone)]
 pub struct Triangle {
-    v0: Vec3,
-    v1: Vec3,
-    v2: Vec3,
+    pub v0: Vec3,
+    pub v1: Vec3,
+    pub v2: Vec3,
 }
 
 impl Triangle {
@@ -104,69 +104,153 @@ impl From<&Mesh> for BoundingSphere {
     }
 }
 
+pub enum RaycastAlgorithm {
+    Geometric,
+    MollerTrumbore(Backfaces),
+}
+
+impl Default for RaycastAlgorithm {
+    fn default() -> Self {
+        RaycastAlgorithm::MollerTrumbore(Backfaces::Cull)
+    }
+}
+
+pub enum Backfaces {
+    Cull,
+    Include,
+}
+
 /// Takes a ray and triangle and computes the intersection and normal
-pub fn ray_triangle_intersection(ray: Ray3D, triangle: Triangle) -> Option<Ray3D> {
+pub fn ray_triangle_intersection(
+    ray: &Ray3D,
+    triangle: &Triangle,
+    algorithm: RaycastAlgorithm,
+) -> Option<Ray3D> {
+    match algorithm {
+        RaycastAlgorithm::Geometric => raycast_geometric(ray, triangle),
+        RaycastAlgorithm::MollerTrumbore(backface_culling) => {
+            raycast_moller_trumbore(ray, triangle, backface_culling)
+        }
+    }
+}
+
+/// Implementation of the Möller-Trumbore ray-triangle intersection test
+pub fn raycast_moller_trumbore(
+    ray: &Ray3D,
+    triangle: &Triangle,
+    backface_culling: Backfaces,
+) -> Option<Ray3D> {
+    // Source: https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/moller-trumbore-ray-triangle-intersection
+    let epsilon: f32 = 0.000001;
+    let vector_v0_to_v1: Vec3 = triangle.v1 - triangle.v0;
+    let vector_v0_to_v2: Vec3 = triangle.v2 - triangle.v0;
+    let p_vec: Vec3 = ray.direction.cross(vector_v0_to_v2);
+    let determinant: f32 = vector_v0_to_v1.dot(p_vec);
+
+    match backface_culling {
+        Backfaces::Cull => {
+            // if the determinant is negative the triangle is backfacing
+            // if the determinant is close to 0, the ray misses the triangle
+            // This test checks both cases
+            if determinant < epsilon {
+                return None;
+            }
+        }
+        Backfaces::Include => {
+            // ray and triangle are parallel if det is close to 0
+            if determinant.abs() < epsilon {
+                return None;
+            }
+        }
+    }
+
+    let determinant_inverse = 1.0 / determinant;
+
+    let t_vec: Vec3 = ray.origin - triangle.v0;
+    let u = t_vec.dot(p_vec) * determinant_inverse;
+    if u < 0.0 || u > 1.0 {
+        return None;
+    }
+
+    let q_vec = t_vec.cross(vector_v0_to_v1);
+    let v = ray.direction.dot(q_vec) * determinant_inverse;
+    if v < 0.0 || u + v > 1.0 {
+        return None;
+    }
+
+    // The distance between ray origin and intersection is t.
+    let t: f32 = vector_v0_to_v2.dot(q_vec) * determinant_inverse;
+
+    // Move along the ray direction from the origin, to find the intersection
+    let point_intersection = ray.origin + ray.direction * t;
+    let triangle_normal = vector_v0_to_v1.cross(vector_v0_to_v2);
+
+    return Some(Ray3D {
+        origin: point_intersection,
+        direction: triangle_normal.normalize(),
+    });
+}
+
+/// Geometric method of computing a ray-triangle intersection
+pub fn raycast_geometric(ray: &Ray3D, triangle: &Triangle) -> Option<Ray3D> {
     // Source: https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/ray-triangle-intersection-geometric-solution
-    let dir = ray.direction;
-    let pos = ray.position;
-    let (v0, v1, v2) = triangle.vertices();
     let epsilon = 0.000001;
 
     // compute plane's normal
-    let v0v1: Vec3 = v1 - v0;
-    let v0v2: Vec3 = v2 - v0;
+    let vector_v0_to_v1: Vec3 = triangle.v1 - triangle.v0;
+    let vector_v0_to_v2: Vec3 = triangle.v2 - triangle.v0;
     // no need to normalize
-    let triangle_normal = v0v1.cross(v0v2); // N
+    let triangle_normal = vector_v0_to_v1.cross(vector_v0_to_v2); // N
 
     // Step 1: finding P
 
     // check if ray and plane are parallel ?
-    let n_dot_ray_direction = triangle_normal.dot(dir);
+    let n_dot_ray_direction = triangle_normal.dot(ray.direction);
     if n_dot_ray_direction.abs() < epsilon {
         return None;
     }
 
     // compute d parameter using equation 2
-    let d = triangle_normal.dot(v0);
+    let d = triangle_normal.dot(triangle.v0);
 
     // compute t (equation 3)
-    let t = (triangle_normal.dot(pos) + d) / n_dot_ray_direction;
+    let t = (triangle_normal.dot(ray.origin) + d) / n_dot_ray_direction;
     // check if the triangle is in behind the ray
     if t < 0.0 {
         return None;
     } // the triangle is behind
 
     // compute the intersection point using equation 1
-    let point_intersection = pos + t * dir;
+    let point_intersection = ray.origin + t * ray.direction;
 
     // Step 2: inside-outside test
 
     // edge 0
-    let edge0 = v1 - v0;
-    let vp0 = point_intersection - v0;
+    let edge0 = triangle.v1 - triangle.v0;
+    let vp0 = point_intersection - triangle.v0;
     let cross = edge0.cross(vp0);
     if triangle_normal.dot(cross) < 0.0 {
         return None;
     } // P is on the right side
 
     // edge 1
-    let edge1 = v2 - v1;
-    let vp1 = point_intersection - v1;
+    let edge1 = triangle.v2 - triangle.v1;
+    let vp1 = point_intersection - triangle.v1;
     let cross = edge1.cross(vp1);
     if triangle_normal.dot(cross) < 0.0 {
         return None;
     } // P is on the right side
 
     // edge 2
-    let edge2 = v0 - v2;
-    let vp2 = point_intersection - v2;
+    let edge2 = triangle.v0 - triangle.v2;
+    let vp2 = point_intersection - triangle.v2;
     let cross = edge2.cross(vp2);
     if triangle_normal.dot(cross) < 0.0 {
         return None;
     } // P is on the right side;
 
     return Some(Ray3D {
-        position: point_intersection,
-        direction: triangle_normal,
+        origin: point_intersection,
+        direction: triangle_normal.normalize(),
     });
 }
