d9215f5122a6bd82790d53f25f41ac3984ed18ae
Aevyrie <aevyrie@gmail.com>
2020-09-17 22:27:40 -0700

    Added stress test

---
+++ examples/stress_test.rs
@@ -0,0 +1,63 @@
+use bevy::{
+    prelude::*,
+    diagnostic::{FrameTimeDiagnosticsPlugin, PrintDiagnosticsPlugin},
+};
+use bevy_mod_picking::*;
+
+fn main() {
+    App::build()
+        .add_resource(Msaa { samples: 4 })
+        .add_default_plugins()
+        .add_plugin(PickingPlugin)
+        .add_plugin(DebugPickingPlugin)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(PrintDiagnosticsPlugin::default())
+        .add_startup_system(setup.system())
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let camera_entity = Entity::new();
+    let edge_length: usize = 10;
+    let subdivision: usize = 40;
+    println!("Tris per mesh: {}",(subdivision+1).pow(2)*20);
+    println!("Total tris: {}",(subdivision+1).pow(2)*20*edge_length.pow(3));
+    for i in 0..edge_length.pow(3) {
+        let f_edge_length = edge_length as f32;
+        let _a = commands
+            .spawn(PbrComponents {
+                mesh: meshes.add(Mesh::from(shape::Icosphere {
+                    radius: 0.25,
+                    subdivisions: subdivision,
+                })),
+                material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+                transform: Transform::from_translation(Vec3::new(i as f32 % f_edge_length - f_edge_length/2.0, (i as f32/f_edge_length).round()%f_edge_length - f_edge_length/2.0, (i as f32/(f_edge_length*f_edge_length)).round()%f_edge_length - f_edge_length/2.0)),
+                ..Default::default()
+            })
+            .with(PickableMesh::new(camera_entity))
+            .with(HighlightablePickMesh::new())
+            .with(SelectablePickMesh::new());
+    }
+    commands
+        .spawn(LightComponents {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        })
+        // camera
+        .spawn_as_entity(
+            camera_entity,
+            Camera3dComponents {
+                transform: Transform::new(Mat4::face_toward(
+                    Vec3::new(edge_length as f32*1.2, edge_length as f32*0.8, edge_length as f32*1.2),
+                    Vec3::new(0.0, -(edge_length as f32)*0.1, 0.0),
+                    Vec3::new(0.0, 1.0, 0.0),
+                )),
+                ..Default::default()
+            },
+        );
+}
