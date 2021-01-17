6f6c35bb67fd8340fca5f882b7f82a64b426ef63
Aevyrie Roessler <aevyrie@gmail.com>
2021-01-17 11:20:11 -0800

    Change camera motion to animated FOV

--- examples/3d_scene.rs
+++ examples/3d_scene.rs
@@ -79,8 +79,10 @@ fn set_highlight_params(mut highlight_params: ResMut<PickHighlightParams>) {
     highlight_params.set_selection_color(Color::rgb(1.0, 0.0, 1.0));
 }
 
-fn oscillation_system(time: Res<Time>, mut query: Query<&mut Transform, With<Camera>>) {
-    for mut transform in query.iter_mut() {
-        transform.translation.y = 5.0 + time.seconds_since_startup().sin() as f32;
+fn oscillation_system(time: Res<Time>, window: Res<Windows>, mut query: Query<&mut Camera>) {
+    for mut camera in query.iter_mut() {
+        let aspect_ratio = window.get_primary().unwrap().width()/window.get_primary().unwrap().height();
+        let oscillator = 3.14/6.0 + (((time.seconds_since_startup()*0.5).sin() + 1.0) * 3.14/4.0) as f32;
+        camera.projection_matrix = Mat4::perspective_rh(oscillator, aspect_ratio, 1.0, 1000.0);
     }
 }
