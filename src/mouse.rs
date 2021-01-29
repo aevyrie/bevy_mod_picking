38caf3a2ee4ac09fc1f4cb4b015afb8a14c257f9
Aevyrie Roessler <aevyrie@gmail.com>
2021-01-28 16:14:42 -0800

    Add mouse updates

---
+++ src/mouse.rs
@@ -0,0 +1,51 @@
+use crate::{PickingCamera, UpdatePicks};
+use bevy::{prelude::*, render::camera::Camera};
+use bevy_mod_raycast::RayCastMethod;
+
+/// Update Screenspace ray cast sources with the current mouse position
+pub fn update_pick_source_positions(
+    mut cursor: EventReader<CursorMoved>,
+    mut pick_source_query: Query<(
+        &mut PickingCamera,
+        Option<&mut UpdatePicks>,
+        Option<&Camera>,
+    )>,
+) {
+    for (mut pick_source, option_update_picks, option_camera) in &mut pick_source_query.iter_mut() {
+        let camera = match option_camera {
+            Some(camera) => camera,
+            None => panic!("The PickingCamera entity has no associated Camera component"),
+        };
+        let mut update_picks = match option_update_picks {
+            Some(update_picks) => update_picks,
+            None => panic!("The PickingCamera entity has no associated UpdatePicks component"),
+        };
+        let cursor_latest = match cursor.iter().last() {
+            Some(cursor_moved) => {
+                if cursor_moved.id == camera.window {
+                    Some(cursor_moved)
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+        match update_picks.clone() {
+            UpdatePicks::EveryFrame(cached_cursor_pos) => {
+                match cursor_latest {
+                    Some(cursor_moved) => {
+                        pick_source.cast_method = RayCastMethod::Screenspace(cursor_moved.position);
+                        *update_picks = UpdatePicks::EveryFrame(cursor_moved.position);
+                    }
+                    None => pick_source.cast_method = RayCastMethod::Screenspace(cached_cursor_pos),
+                };
+            }
+            UpdatePicks::OnMouseEvent => match cursor_latest {
+                Some(cursor_moved) => {
+                    pick_source.cast_method = RayCastMethod::Screenspace(cursor_moved.position)
+                }
+                None => continue,
+            },
+        };
+    }
+}
