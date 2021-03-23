4d42add372e1391e8dd7ccf8fc955b8a1de38d52
Aevyrie Roessler <aevyrie@gmail.com>
2021-03-23 01:54:48 -0700

    Re-export RayCastSource

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub use crate::{
     mouse::update_pick_source_positions,
     selection::{mesh_selection, NoDeselect, Selection},
 };
-pub use bevy_mod_raycast::BoundVol;
+pub use bevy_mod_raycast::{BoundVol, RayCastSource};
 
 use bevy::{prelude::*, ui::FocusPolicy};
 
