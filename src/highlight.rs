9a0b34eece20243fc8f59d6613772aba50e3bab5
Aevyrie <aevyrie@gmail.com>
2021-05-16 20:20:58 -0700

    Add message to unwrap

--- src/highlight.rs
+++ src/highlight.rs
@@ -16,7 +16,7 @@ impl FromWorld for MeshButtonMaterials {
     fn from_world(world: &mut World) -> Self {
         let mut materials = world
             .get_resource_mut::<Assets<StandardMaterial>>()
-            .unwrap();
+            .expect("Failed to get resource");
         MeshButtonMaterials {
             hovered: materials.add(Color::rgb(0.35, 0.35, 0.35).into()),
             pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
