f711aed911627e8575d0b96e50c9a0d9743c41e9
Aevyrie Roessler <aevyrie@gmail.com>
2022-04-17 17:38:56 -0700

    Add events

--- examples/multiple_windows.rs
+++ examples/multiple_windows.rs
@@ -10,7 +10,9 @@ use bevy::{
     },
     window::{CreateWindow, WindowId},
 };
-use bevy_mod_picking::{DebugCursorPickingPlugin, DefaultPickingPlugins, PickableBundle};
+use bevy_mod_picking::{
+    DebugCursorPickingPlugin, DebugEventsPickingPlugin, DefaultPickingPlugins, PickableBundle,
+};
 
 /// This example creates a second window and draws a mesh from two different cameras, one in each window
 fn main() {
@@ -18,7 +20,8 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(SecondWindowCameraPlugin)
         .add_plugins(DefaultPickingPlugins)
-        .add_plugin(DebugCursorPickingPlugin)
+        .add_plugin(DebugCursorPickingPlugin) // <- Adds the green debug cursor.
+        .add_plugin(DebugEventsPickingPlugin) // <- Adds debug event logging.
         .add_startup_system(setup)
         .add_startup_system(create_new_window)
         .run();
