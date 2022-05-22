a9fe29332aa6f7a969fd995d5e4cc6fd84531eef
Aevyrie Roessler <aevyrie@gmail.com>
2022-05-22 01:39:13 -0700

    highlighting refactor and 2d support

--- src/highlight.rs
+++ src/highlight.rs
@@ -1,41 +1,52 @@
-use std::marker::PhantomData;
-
 use super::selection::*;
 use crate::PausedForBlockers;
 use bevy::{asset::Asset, prelude::*, render::color::Color};
 
-/// A default highlightable button material implementation of the [`IsPickableButton`] trait that
-/// uses bevy's [`StandardMaterial`] for highlighting meshes. You may want to implement your own
-/// component if the pickable object being rendered is not a mesh, or doesn't use the
-/// `StandardMaterial` component for rendered appearance.
+/// Marker component to flag an entity as highlightable
 #[derive(Component, Clone, Debug, Default)]
-pub struct PickableButton<T: Asset> {
-    pub initial: Option<Handle<T>>,
+pub struct Highlight;
+
+/// Component used to track the initial asset of a highlightable object, as well as for overriding
+/// the default highlight materials.
+#[derive(Component, Clone, Debug)]
+pub struct Highlighting<T: Asset> {
+    pub initial: Handle<T>,
     pub hovered: Option<Handle<T>>,
     pub pressed: Option<Handle<T>>,
     pub selected: Option<Handle<T>>,
 }
 
-pub struct MeshButtonMaterials<T: Asset, U: FromWorldHelper<T> + ?Sized> {
-    pub phantom: PhantomData<U>,
-    pub hovered: Handle<T>,
-    pub pressed: Handle<T>,
-    pub selected: Handle<T>,
+/// Resource that defines the default highlighting assets to use. This can be overridden per-entity
+/// with the [`Highlighting`] component.
+pub struct DefaultHighlighting<T: Highlightable + ?Sized> {
+    pub hovered: Handle<T::HighlightAsset>,
+    pub pressed: Handle<T::HighlightAsset>,
+    pub selected: Handle<T::HighlightAsset>,
 }
 
-pub trait FromWorldHelper<T: Asset>: Default {
-    fn from_world_helper(world: &mut World) -> MeshButtonMaterials<T, Self>;
+/// This trait makes it possible for highlighting to be generic over any type of asset.
+pub trait Highlightable: Default {
+    /// The asset used to highlight the picked object. For a 3D mesh, this would probably be [`StandardMaterial`].
+    type HighlightAsset: Asset;
+    fn highlight_defaults(
+        materials: Mut<Assets<Self::HighlightAsset>>,
+    ) -> DefaultHighlighting<Self>;
+    fn materials(world: &mut World) -> Mut<Assets<Self::HighlightAsset>> {
+        world
+            .get_resource_mut::<Assets<Self::HighlightAsset>>()
+            .expect("Failed to get resource")
+    }
 }
 
 #[derive(Default)]
-pub struct StandardMaterialPickingColors;
-impl FromWorldHelper<StandardMaterial> for StandardMaterialPickingColors {
-    fn from_world_helper(world: &mut World) -> MeshButtonMaterials<StandardMaterial, Self> {
-        let mut materials = world
-            .get_resource_mut::<Assets<StandardMaterial>>()
-            .expect("Failed to get resource");
-        MeshButtonMaterials {
-            phantom: PhantomData::default(),
+pub struct StandardMaterialHighlight;
+impl Highlightable for StandardMaterialHighlight {
+    type HighlightAsset = StandardMaterial;
+
+    fn highlight_defaults(
+        mut materials: Mut<Assets<Self::HighlightAsset>>,
+    ) -> DefaultHighlighting<Self> {
+        DefaultHighlighting {
             hovered: materials.add(Color::rgb(0.35, 0.35, 0.35).into()),
             pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
             selected: materials.add(Color::rgb(0.35, 0.35, 0.75).into()),
@@ -43,36 +54,60 @@ impl FromWorldHelper<StandardMaterial> for StandardMaterialPickingColors {
     }
 }
 
-impl<T: Asset, U: FromWorldHelper<T>> FromWorld for MeshButtonMaterials<T, U> {
+#[derive(Default)]
+pub struct ColorMaterialHighlight;
+impl Highlightable for ColorMaterialHighlight {
+    type HighlightAsset = ColorMaterial;
+
+    fn highlight_defaults(
+        mut materials: Mut<Assets<Self::HighlightAsset>>,
+    ) -> DefaultHighlighting<Self> {
+        DefaultHighlighting {
+            hovered: materials.add(Color::rgb(0.35, 0.35, 0.35).into()),
+            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
+            selected: materials.add(Color::rgb(0.35, 0.35, 0.75).into()),
+        }
+    }
+}
+
+impl<T: Highlightable> FromWorld for DefaultHighlighting<T> {
     fn from_world(world: &mut World) -> Self {
-        U::from_world_helper(world)
+        T::highlight_defaults(T::materials(world))
     }
 }
 
 #[allow(clippy::type_complexity)]
-pub fn get_initial_mesh_button_material<T: Asset>(
-    mut query: Query<
-        (&mut PickableButton<T>, &Handle<T>),
-        Or<(Added<PickableButton<T>>, Changed<Handle<T>>)>,
-    >,
+pub fn get_initial_mesh_highlight_asset<T: Asset>(
+    mut commands: Commands,
+    entity_asset_query: Query<(Entity, &Handle<T>), Added<Highlight>>,
+    mut highlighting_query: Query<Option<&mut Highlighting<T>>>,
 ) {
-    for (mut button, material) in query.iter_mut() {
-        if button.initial.is_none() {
-            button.initial = Some(material.clone());
+    for (entity, material) in entity_asset_query.iter() {
+        match highlighting_query.get_mut(entity) {
+            Ok(Some(mut highlighting)) => highlighting.initial = material.to_owned(),
+            _ => {
+                let init_component = Highlighting {
+                    initial: material.to_owned(),
+                    hovered: None,
+                    pressed: None,
+                    selected: None,
+                };
+                commands.entity(entity).insert(init_component);
+            }
         }
     }
 }
 
 #[allow(clippy::type_complexity)]
-pub fn mesh_highlighting<T: Asset, U: 'static + FromWorldHelper<T> + Send + Sync>(
+pub fn mesh_highlighting<T: 'static + Highlightable + Send + Sync>(
     paused: Option<Res<PausedForBlockers>>,
-    global_button_materials: Res<MeshButtonMaterials<T, U>>,
+    global_default_highlight: Res<DefaultHighlighting<T>>,
     mut interaction_query: Query<
         (
             &Interaction,
-            &mut Handle<T>,
+            &mut Handle<T::HighlightAsset>,
             Option<&Selection>,
-            &PickableButton<T>,
+            &Highlighting<T::HighlightAsset>,
         ),
         Or<(Changed<Interaction>, Changed<Selection>)>,
     >,
@@ -80,66 +115,49 @@ pub fn mesh_highlighting<T: Asset, U: 'static + FromWorldHelper<T> + Send + Sync
     // Set non-hovered material when picking is paused (e.g. while hovering a picking blocker).
     if let Some(paused) = paused {
         if paused.is_paused() {
-            for (_, mut material, selection, button) in interaction_query.iter_mut() {
-                let try_material = if let Some(selection) = selection {
-                    if selection.selected() {
-                        if let Some(button_material) = &button.selected {
-                            Some(button_material.clone())
-                        } else {
-                            Some(global_button_materials.selected.clone())
-                        }
+            for (_, mut material, selection, highlight) in interaction_query.iter_mut() {
+                *material = if selection.filter(|s| s.selected()).is_some() {
+                    if let Some(highlight_asset) = &highlight.selected {
+                        highlight_asset
                     } else {
-                        button.initial.clone()
+                        &global_default_highlight.selected
                     }
                 } else {
-                    button.initial.clone()
-                };
-                if let Some(m) = try_material {
-                    *material = m;
-                } else {
-                    warn!("Selectable entity missing its initial material");
+                    &highlight.initial
                 }
+                .to_owned();
             }
             return;
         }
     }
-    for (interaction, mut material, selection, button) in interaction_query.iter_mut() {
-        let try_material = match *interaction {
+    for (interaction, mut material, selection, highlight) in interaction_query.iter_mut() {
+        *material = match *interaction {
             Interaction::Clicked => {
-                if let Some(button_material) = &button.pressed {
-                    Some(button_material.clone())
+                if let Some(highlight_asset) = &highlight.pressed {
+                    highlight_asset
                 } else {
-                    Some(global_button_materials.pressed.clone())
+                    &global_default_highlight.pressed
                 }
             }
             Interaction::Hovered => {
-                if let Some(button_material) = &button.hovered {
-                    Some(button_material.clone())
+                if let Some(highlight_asset) = &highlight.hovered {
+                    highlight_asset
                 } else {
-                    Some(global_button_materials.hovered.clone())
+                    &global_default_highlight.hovered
                 }
             }
             Interaction::None => {
-                if let Some(selection) = selection {
-                    if selection.selected() {
-                        if let Some(button_material) = &button.selected {
-                            Some(button_material.clone())
-                        } else {
-                            Some(global_button_materials.selected.clone())
-                        }
+                if selection.filter(|s| s.selected()).is_some() {
+                    if let Some(highlight_asset) = &highlight.selected {
+                        highlight_asset
                     } else {
-                        button.initial.clone()
+                        &global_default_highlight.selected
                     }
                 } else {
-                    button.initial.clone()
+                    &highlight.initial
                 }
             }
-        };
-
-        if let Some(m) = try_material {
-            *material = m;
-        } else {
-            warn!("Selectable entity missing its initial material");
         }
+        .to_owned();
     }
 }
