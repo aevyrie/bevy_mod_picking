c3c1b8c8cbb79a245643e866f1853853841215e9
Aevyrie <aevyrie@gmail.com>
2023-05-03 02:24:01 -0700

    fix sprite deps

--- crates/bevy_picking_highlight/src/lib.rs
+++ crates/bevy_picking_highlight/src/lib.rs
@@ -39,6 +39,7 @@ impl Plugin for DefaultHighlightingPlugin {
             },
         });
 
+        #[cfg(feature = "sprite")]
         app.add_plugin(HighlightPlugin::<bevy::sprite::ColorMaterial> {
             highlighting_default: |mut assets| GlobalHighlight {
                 hovered: assets.add(Color::rgb(0.35, 0.35, 0.35).into()),
