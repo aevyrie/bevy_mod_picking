36da26852e9ef71ae54cff7a3f740afb271f02d5
Aevyrie <aevyrie@gmail.com>
2023-07-13 21:41:00 -0700

    Fix viewport bug with backend

--- backends/bevy_picking_rapier/src/lib.rs
+++ backends/bevy_picking_rapier/src/lib.rs
@@ -49,9 +49,7 @@ impl RapierPickCamera {
 /// Updates all picking [`Ray`]s with [`PointerLocation`]s.
 pub fn build_rays_from_pointers(
     pointers: Query<(&PointerId, &PointerLocation)>,
-    windows: Query<&Window>,
     primary_window: Query<Entity, With<PrimaryWindow>>,
-    images: Res<Assets<Image>>,
     mut picking_cameras: Query<(&Camera, &GlobalTransform, &mut RapierPickCamera)>,
 ) {
     picking_cameras.iter_mut().for_each(|(_, _, mut pick_cam)| {
@@ -64,9 +62,7 @@ pub fn build_rays_from_pointers(
         };
         picking_cameras
             .iter_mut()
-            .filter(|(camera, _, _)| {
-                pointer_location.is_in_viewport(camera, &windows, &primary_window, &images)
-            })
+            .filter(|(camera, _, _)| pointer_location.is_in_viewport(camera, &primary_window))
             .for_each(|(camera, transform, mut source)| {
                 let pointer_pos = pointer_location.position;
                 if let Some(ray) = camera.viewport_to_world(transform, pointer_pos) {
