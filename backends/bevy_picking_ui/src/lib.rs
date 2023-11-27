4df4b1c1f3d1df86db86acc2fd4c21f2088e481b
ickshonpe <david.curthoys@googlemail.com>
2023-11-27 00:27:14 +0000

    The Bevy UI backend doesn't check for clipping, and allows interaction with clipped non-visible sections of UI nodes.
    This PR intersects the node rect with its calculated clip rect (if it has one) and then checks if the clipped node contains the cursor.

--- backends/bevy_picking_ui/src/lib.rs
+++ backends/bevy_picking_ui/src/lib.rs
@@ -68,6 +68,7 @@ pub fn ui_picking(
     cameras: Query<(Entity, &Camera, Option<&UiCameraConfig>)>,
     primary_window: Query<Entity, With<PrimaryWindow>>,
     ui_stack: Res<UiStack>,
+    ui_scale: Res<UiScale>,
     mut node_query: Query<NodeQuery>,
     mut output: EventWriter<PointerHits>,
 ) {
@@ -120,24 +121,13 @@ pub fn ui_picking(
                         }
                     }
 
-                    let position = node.global_transform.translation();
-                    let ui_position = position.truncate();
-                    let extents = node.node.size() / 2.0;
-                    let mut min = ui_position - extents;
-                    if let Some(clip) = node.calculated_clip {
-                        min = Vec2::max(min, clip.clip.min);
-                    }
-
-                    // The mouse position relative to the node
-                    // (0., 0.) is the top-left corner, (1., 1.) is the bottom-right corner
-                    let relative_cursor_position = Vec2::new(
-                        (location.position.x - min.x) / node.node.size().x,
-                        (location.position.y - min.y) / node.node.size().y,
-                    );
-
-                    if (0.0..1.).contains(&relative_cursor_position.x)
-                        && (0.0..1.).contains(&relative_cursor_position.y)
-                    {
+                    let node_rect = node.node.logical_rect(node.global_transform);
+                    let visible_rect = node
+                        .calculated_clip
+                        .map(|clip| node_rect.intersect(clip.clip))
+                        .unwrap_or(node_rect);
+                    let cursor_position = location.position / ui_scale.0 as f32;
+                    if visible_rect.contains(cursor_position) {
                         Some(*entity)
                     } else {
                         None
