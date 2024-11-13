622aa66c3121e9f1ef941dc86cf73e5e4cd4091b
Michael Walter Van Der Velden <mikevdv@vandie.co.uk>
2024-11-13 19:54:47 +0000

    remove bang op for clarity

--- backends/bevy_picking_sprite/src/lib.rs
+++ backends/bevy_picking_sprite/src/lib.rs
@@ -151,8 +151,8 @@ pub fn sprite_picking(
                     let is_cursor_in_sprite = rect.contains(cursor_pos_sprite.truncate());
 
                     let cursor_in_valid_pixels_of_sprite = is_cursor_in_sprite
-                        && (!settings.passthrough_transparency
-                            || image.is_some() && {
+                        && (settings.passthrough_transparency == false
+                            || (image.is_some() && {
                                 let texture: &Image = image.and_then(|i| images.get(i))?;
                                 // If using a texture atlas, grab the offset of the current sprite index. (0,0) otherwise
                                 let texture_rect = atlas
@@ -183,7 +183,7 @@ pub fn sprite_picking(
                                 } else {
                                     false
                                 }
-                            });
+                            }));
 
                     blocked = cursor_in_valid_pixels_of_sprite
                         && pickable.map(|p| p.should_block_lower) != Some(false);
